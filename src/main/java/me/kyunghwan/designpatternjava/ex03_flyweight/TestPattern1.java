30b7c270df9959f6f2f9a01dc3685059147598ee
Kyunghwan <rudghks373@naver.com>
2020-12-28 13:27:39 +0900

    02.Flyweight Pattern - 기존 자바 코드에서 볼 수 있는 플라이웨이트 패턴

---
+++ src/main/java/me/kyunghwan/designpatternjava/ex03_flyweight/TestPattern1.java
@@ -0,0 +1,15 @@
+package me.kyunghwan.designpatternjava.ex03_flyweight;
+
+public class TestPattern1 {
+
+    public static void main(String[] args) {
+        String str1 = new String("테스트");
+        String str2 = new String("테스트");
+        String str3 = "테스트";
+        String str4 = "테스트";
+
+        System.out.println("Flyweight Pattern");
+
+    }
+
+}
---
+++ src/main/java/me/kyunghwan/designpatternjava/ex03_flyweight/TestPattern2.java
@@ -0,0 +1,35 @@
+package me.kyunghwan.designpatternjava.ex03_flyweight;
+
+public class TestPattern2 {
+
+    public static void main(String[] args) {
+        MyData md1 = new MyData();
+        md1.xpos = 10;
+        md1.ypos = 11;
+        md1.name = "테스트";
+
+        MyData md2 = new MyData();
+        
+        //md2 와 md1가 인스턴스를 공유하고 있음
+        md2 = md1;
+
+        MyData md3 = new MyData();
+        md3.xpos = 20;
+        md3.ypos = 21;
+        md3.name = "홍길동";
+
+
+        //md1,md2와 인스턴스 필드 값이 둘다 변한다.
+        md2.name = "전우치";
+        md2.xpos = 5;
+
+        
+    }
+
+}
+
+class MyData {
+    int xpos;
+    int ypos;
+    String name;
+}
